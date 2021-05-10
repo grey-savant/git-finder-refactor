4eb7126026ac0d0b04b0888a7d353a82342cd814
Jonathan Artemis Pierce <piercesavant@gmail.com>
2021-05-10 11:13:02 -0700

    Refactored repos component to use Context

--- src/App.js
+++ src/App.js
@@ -6,34 +6,11 @@ import User from './components/users/User';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
-import axios from 'axios';
 import GithubState from './context/github/GithubState';
 import 'bootstrap/dist/css/bootstrap.css';
 
 const App = () => {
-  const [ repos, setRepos ] = useState([]);
-  const [ loading, setLoading ] = useState(false);
   const [ alert, setAlert] = useState(null);
-
-  // async componentDidMount() {
-  //   this.setState({ loading: true });
-
-  //   const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    
-  //   this.setState({users: res.data, loading: false});
-
-  //   console.log(res.data);
-  // }
-
-  // Get user repos
-  const getUserRepos = async (username) => {
-    setLoading(true);
-
-    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    
-    setRepos(res.data);
-    setLoading(false);
-  }
 
   // set alert
   const showAlert = (msg, type) => {
@@ -58,13 +35,7 @@ const App = () => {
             </Fragment>
           )}/>
           <Route exact path="/about" component={About}/>
-          <Route exact path="/user/:login" render={props => (
-            <User 
-              { ...props } 
-              getUserRepos={getUserRepos} 
-              repos={repos} 
-             />
-          )}/>
+          <Route exact path="/user/:login" component={User} />
         </Switch>
         
       </div>
--- src/context/github/githubReducer.js
+++ src/context/github/githubReducer.js
@@ -36,6 +36,13 @@ export default ( state, action ) => {
                 loading: true
             };
 
+        case GET_REPOS:
+            return {
+                ...state,
+                repos: action.payload,
+                loading:false
+            }
+
         default:
             return state;
     }
